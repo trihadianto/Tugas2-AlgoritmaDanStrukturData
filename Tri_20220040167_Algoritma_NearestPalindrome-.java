cfbb706984b9298c554568ce517967da64448962
tri hadianto <114473435+trihadianto@users.noreply.github.com>
2023-03-20 09:01:12 +0700

    Update Tri_20220040167_Algoritma_NearestPalindrome-.java

--- Tri_20220040167_Algoritma_NearestPalindrome-.java
+++ Tri_20220040167_Algoritma_NearestPalindrome-.java
@@ -1,30 +1,41 @@
-import java.util.Scanner;
+package TugasSesi4;
 
 
-public class NearestPalindrome {
-
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("======Nearest Palindrome======");
-        System.out.println("Program ini akan meminta input berupa sebuah angka.");
-        System.out.print("Masukkan sebuah angka: ");
-        int angka = scanner.nextInt();
-        int angkaPalindrome = angka;
-        while (!ispalindrome(angkaPalindrome)){
-            angkaPalindrome++;
-        }
-        System.out.println("Angka palindrome terdekat dari " + angka + " adalah " + angkaPalindrome);
-    }
-    
- public static boolean isPalindrome(int angka) {
-        int reversedNumber = 0;
-        int originalNumber = angka;
-        while (angka != 0) {
-            int digit = angka % 10;
-            reversedNumber = reversedNumber * 10 + digit;
-            angka /= 10;
-        }
-        return originalNumber == reservedNumber;
-    }
-}
+public class TugasSesi4 {
+     public static void main(String[] args) {
         
+        System.out.println("=".repeat(18) + " Tugas Sesi 4 " + "=".repeat(18));
+
+        System.out.println("=".repeat(50));
+        
+        char hotel [][] = {
+            
+            // Kamar (Kolom) 1, 2, 3, 4, 5
+            // Lantai (Baris) 1, 2, 3, 4     
+            
+            {'*', '*', '*', 'X', '*'},
+            {'*', '*', '*', '*', '*'},
+            {'*', '*', '*', '*', '*'},
+            {'*', '*', '*', '*', 'X'}
+        };
+        
+        char kamarIsi = 'X';
+        char kamarKosong = '*';
+        int jumlahKamar = hotel.length;
+        int jumlahKamarKosong = 0;
+        
+        for(int lantai = 0; lantai < jumlahKamar; lantai++){
+            for(int kamar = 0; kamar < hotel[lantai].length; kamar++){
+                if(hotel[lantai][kamar] == kamarIsi){
+                    System.out.println("\033[1mTamu berada di Lantai "+ (lantai+1) +" Kamar "+ (kamar+1)+ "\033[0m");
+                }
+                
+                if(hotel[lantai][kamar] == kamarKosong){
+                    jumlahKamarKosong++;
+                }
+            }
+        }
+        System.out.println("\033[1mJumlah kamar yang tersedia adalah " + jumlahKamarKosong + " kamar\033[0m");
+        System.out.println("=".repeat(50));
+    }
+}     
